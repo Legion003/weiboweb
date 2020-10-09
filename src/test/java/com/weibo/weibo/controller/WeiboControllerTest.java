aedba60a606b02b5033831dc32078ed719fc9bb7
Legion <legionxxx@qq.com>
2020-10-09 16:15:29 +0800

    change code

--- src/test/java/com/weibo/weibo/controller/WeiboControllerTest.java
+++ src/test/java/com/weibo/weibo/controller/WeiboControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -37,7 +38,7 @@ public class WeiboControllerTest {
         String keyword = "新冠";
         String keyword_utf8 = URLDecoder.decode(keyword, "UTF-8");
         String get_url = "/getWeibo/" + keyword_utf8 + "/1";
-        String result = mockMvc.perform(MockMvcRequestBuilders.get(get_url))
+        String result = mockMvc.perform(MockMvcRequestBuilders.get(get_url).accept(MediaType.APPLICATION_JSON_UTF8))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andDo(MockMvcResultHandlers.print()).andReturn()
                 .getResponse()
